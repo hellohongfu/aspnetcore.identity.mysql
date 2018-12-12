6bd5021d7e6241b2d78b5f49c075ba90bebcfe42
hellohongfu <hellohongfu@hotmail.com>
2018-12-12 23:21:01 +0800

    添加项目文件。

---
+++ WebMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using WebMvc.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace WebMvc.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+
+
+
+            builder.Entity<ApplicationUser>(b => {
+
+                b.HasKey(p => p.Id);
+                b.Property(p => p.LockoutEnabled).HasColumnType("bit");
+                b.Property(p => p.TwoFactorEnabled).HasColumnType("bit");
+                b.Property(p => p.PhoneNumberConfirmed).HasColumnType("bit");
+                b.Property(p => p.EmailConfirmed).HasColumnType("bit");
+
+            });
+            builder.Entity<IdentityUserLogin<string>>(b => {
+
+                b.HasKey(p => new { p.UserId,p.LoginProvider,p.ProviderKey });
+
+                b.Property(p => p.UserId).HasMaxLength(50);
+                b.Property(p => p.LoginProvider).HasMaxLength(50);
+                b.Property(p => p.ProviderKey).HasMaxLength(50);
+               
+
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(b => {
+
+                b.HasKey(p => new { p.UserId, p.LoginProvider, p.ProviderKey });
+
+                b.Property(p => p.UserId).HasMaxLength(50);
+                b.Property(p => p.LoginProvider).HasMaxLength(50);
+                b.Property(p => p.ProviderKey).HasMaxLength(50);
+
+
+            });
+
+            builder.Entity<IdentityUserRole<string>>(b => {
+
+                b.HasKey(p => new { p.UserId, p.RoleId });
+
+                b.Property(p => p.UserId).HasMaxLength(50);
+                b.Property(p => p.RoleId).HasMaxLength(50);
+
+
+            });
+
+
+            builder.Entity<IdentityUserToken<string>>(b => {
+
+                b.HasKey(p => new { p.UserId, p.LoginProvider,p.Name });
+
+                b.Property(p => p.UserId).HasMaxLength(50);
+                b.Property(p => p.Name).HasMaxLength(50);
+                b.Property(p => p.LoginProvider).HasMaxLength(50);
+
+
+
+            });
+
+
+        }
+
+
+    }
+}
---
+++ WebMvc/Models/ApplicationUser.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace WebMvc.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+        //[System.ComponentModel.DataAnnotations.Schema.Column(TypeName = "bit")]
+        //public override bool LockoutEnabled { get => base.LockoutEnabled; set => base.LockoutEnabled = value; }
+        //[System.ComponentModel.DataAnnotations.Schema.Column(TypeName = "bit")]
+        //public override bool TwoFactorEnabled { get => base.TwoFactorEnabled; set => base.TwoFactorEnabled = value; }
+        //[System.ComponentModel.DataAnnotations.Schema.Column(TypeName = "bit")]
+        //public override bool EmailConfirmed { get => base.EmailConfirmed; set => base.EmailConfirmed = value; }
+        //[System.ComponentModel.DataAnnotations.Schema.Column(TypeName = "bit")]
+        //public override bool PhoneNumberConfirmed { get => base.PhoneNumberConfirmed; set => base.PhoneNumberConfirmed = value; }
+    }
+}
