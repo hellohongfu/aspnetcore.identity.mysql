967e583de18302171bde3c562f8ebc70e3fc6c6f
hellohongfu <hellohongfu@hotmail.com>
2018-12-15 02:37:01 +0800

    内存配置项加载 json配置项加载

--- WebMvc/Controllers/HomeController.cs
+++ WebMvc/Controllers/HomeController.cs
@@ -5,13 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebMvc.Models;
+using Microsoft.Extensions.Configuration;
+
 
 namespace WebMvc.Controllers
 {
     public class HomeController : Controller
     {
+        public class ArrayExample
+        {
+            public string[] Entries { get; set; }
+        }
+        private IConfiguration _config;
+
+        public HomeController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+
         public IActionResult Index()
         {
+            var arrayExample = new ArrayExample();
+            _config.GetSection("array").Bind(arrayExample);
+
+            ArrayExample ArrayExample = _config.GetSection("array").Get<ArrayExample>();
+            var entries = _config.GetValue<Array>("entries");
+
+
+
             return View();
         }
 
--- WebMvc/Program.cs
+++ WebMvc/Program.cs
@@ -12,6 +12,14 @@ namespace WebMvc
 {
     public class Program
     {
+        public static Dictionary<string, string> arrayDict = new Dictionary<string, string>
+        {
+            {"array:entries:0", "value0"},
+            {"array:entries:1", "value1"},
+            {"array:entries:2", "value2"},
+            {"array:entries:4", "value4"},
+            {"array:entries:5", "value5"}
+        };
         public static void Main(string[] args)
         {
             BuildWebHost(args).Run();
@@ -19,6 +27,15 @@ namespace WebMvc
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostContext, config) =>
+            {
+                config.AddInMemoryCollection(arrayDict);
+               // config.AddJsonFile("json_array.json", optional: false, reloadOnChange: false);
+                //config.AddJsonFile("starship.json", optional: false, reloadOnChange: false);
+                //config.AddXmlFile("tvshow.xml", optional: false, reloadOnChange: false);
+                //config.AddEFConfiguration(options => options.UseInMemoryDatabase("InMemoryDb"));
+                config.AddCommandLine(args);
+            })
                 .UseStartup<Startup>()
                 .Build();
     }
